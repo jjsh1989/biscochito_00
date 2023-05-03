4f5606fd8d2ea8b8c6742460d5fad4a769c13602
jjsh1989 <jjsh1989@gmail.com>
2023-05-03 15:41:44 +0200

    Update sketch_apr21a.ino

--- sketch_apr21a.ino
+++ sketch_apr21a.ino
@@ -1,28 +1,31 @@
 // Bibliotecas
   #include <esp_system.h>
   #include <M5StickCPlus.h>
-
   #include "Variables.h"
   #include "LCD.h"
 
 void setup() {
-  M5.begin(); 	                                                                        // Inicializa todas las funciones del Modulo
+// Inicia las funciones del Modulo M5
+  M5.begin(); 	                                                                      // Inicializa todas las funciones del Modulo
 
 // IO Config
-  pinMode(PIN_LED, OUTPUT);                                                             // Configuramos el pin como salida
-  digitalWrite(PIN_LED, HIGH);                                                          // Apagamos el LED al inicio
+  pinMode(PIN_LED, OUTPUT);                                                           // Configuramos el pin como salida
+  digitalWrite(PIN_LED, HIGH);                                                        // Apagamos el LED al inicio
+  
 // Lcd
   LcdConfig();
   LcdMarco(Text01);
   LcdTexto_01(3, 2); 
+  
 }
 
 void loop() {
-  M5.update();                                                                          // Permite leer el estado de los pulsadores
+  M5.update();                                                                        // Permite leer el estado de los pulsadores
   tiempoActual = millis();
 
 if (M5.BtnB.pressedFor(2000)) {                                                       // Funcion Reset
   ESP.restart();
+  
   }
 
 if (AnimacionOn == 1){
@@ -33,6 +36,7 @@ if (AnimacionOn == 1){
       LcdMarco(Text01);
       LcdTexto_02("5", 7, 50);
       Menu_01 = 2;
+        
       }
       break;
     case 2:                                                                           // Animacion "4"
@@ -41,6 +45,7 @@ if (AnimacionOn == 1){
       LcdMarco(Text01);
       LcdTexto_02("4", 7, 50);
       Menu_01 = 3;
+        
       }
       break;
     case 3:                                                                           // Animacion "3"
@@ -49,6 +54,7 @@ if (AnimacionOn == 1){
       LcdMarco(Text01);
       LcdTexto_02("3", 7, 50);
       Menu_01 = 4;
+        
       }
       break;
     case 4:                                                                           // Animacion "2"
@@ -57,6 +63,7 @@ if (AnimacionOn == 1){
       LcdMarco(Text01);
       LcdTexto_02("2", 7, 50);
       Menu_01 = 5; 
+        
       }
       break;
     case 5:                                                                           // Animacion "1"
@@ -65,6 +72,7 @@ if (AnimacionOn == 1){
       LcdMarco(Text01);
       LcdTexto_02("1", 7, 50);
       Menu_01 = 6; 
+        
       }
       break;
     case 6:                                                                           // Animacion "Start"
@@ -72,7 +80,8 @@ if (AnimacionOn == 1){
       ultimoTiempoActualizado = tiempoActual;
       LcdMarco(Text01);
       LcdTexto_02("Start", 4, 10);
-      Menu_01 = 7; 
+      Menu_01 = 7;
+        
       }
       break;
     case 7:                                                                           // Animacion "Inicio"
@@ -85,6 +94,7 @@ if (AnimacionOn == 1){
       BlinkOn = 1;
       BotonesOn = 1;
       Menu_01 = 10;
+        
       } 
       break;
     default:                                                                          // Espera para comenzar la animacion
@@ -92,11 +102,13 @@ if (AnimacionOn == 1){
         if (millis() - tiempo_anterior_01 >= INTERVALO_01) {
         tiempo_anterior_01 += INTERVALO_01;
         Menu_01 = 1; 
+          
         }
       }     
       break;
   }
 }
+  
 if (BlinkOn == 01) {                                                                    // Funcion Blink
   if (millis() - tiempo_anterior_02 >= INTERVALO_02) {                                  // Verificamos si ha pasado el tiempo suficiente para cambiar el estado del LED
     tiempo_anterior_02 += INTERVALO_02;
@@ -105,6 +117,7 @@ if (BlinkOn == 01) {                                                            
 
     }
   } 
+  
 if (BotonesOn == 1){                                                                    // Funciones vinculadas a los botones
   if (M5.BtnA.wasReleased()) {                                                          // Funcion "+"   
     incrementar(INTERVALO_02);                                                          // Funcion incremental
@@ -112,13 +125,18 @@ if (BotonesOn == 1){                                                            
     LcdMarco(Text01);
     LcdTexto_03(Tiempo);
     LcdTexto_04();
+    
     }
+  
   if (M5.BtnB.wasReleased()) {                                                          // Funcion "-"
     decrementar(INTERVALO_02);                                                          // Funcion Decremental
     Tiempo = INTERVALO_02;                                                              // Variable para mostrar                
     LcdMarco(Text01);
     LcdTexto_03(Tiempo);
     LcdTexto_04();
+    
     } 
+  
   }
+  
 }
